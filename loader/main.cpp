b2002e75d69e3cc0204a8ca6d7bf607aac0e6cbf
zonde306 <zonde@139.com>
2019-05-04 16:35:18 +0800

    loader added

---
+++ loader/main.cpp
@@ -0,0 +1,40 @@
+#include <Windows.h>
+#include <iostream>
+#include "proccess.h"
+
+int main(int argc, char** argv)
+{
+	std::string procName = u8"left4dead2.exe";
+	if (argc >= 2)
+		procName = argv[1];
+
+	std::string fileName = proc::GetCurrentPath(argv[0]) + u8"\\l4d2Simple2.dll";
+	if (argc >= 3)
+		fileName = argv[2];
+
+	DWORD pid = proc::FindProccess(procName);
+	if (pid == NULL)
+	{
+		std::cout << procName << " not found." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	DWORD result = EXIT_SUCCESS;
+	try
+	{
+		result = proc::InjectDLL(fileName, pid);
+	}
+	catch (const std::exception & error)
+	{
+		std::cout << error.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	
+	std::cout << "proccess: " << procName << std::endl;
+	std::cout << "module: " << fileName << std::endl;
+	std::cout << "pid: " << pid << std::endl;
+	std::cout << "address: " << std::ios::uppercase << std::ios::hex <<
+		proc::GetModuleBase(fileName.substr(fileName.rfind("\\")), pid);
+
+	return result;
+}
---
+++ loader/proccess.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <Windows.h>
+#include <Winternl.h>
+#include <string>
+#include <tuple>
+
+namespace proc
+{
+	DWORD FindProccess(const std::string& procName);
+	DWORD InjectDLL(const std::string& fileName, DWORD pid);
+	std::string GetErrorMessage(DWORD code, const std::string& defaults = "");
+	std::string GetCurrentPath(const std::string& selfPath);
+	DWORD GetModuleBase(const std::string& moduleName, DWORD pid);
+
+#ifdef _WIN64
+	typedef  DWORD64 DWORDX;
+#else
+	typedef  DWORD32 DWORDX;
+#endif
+	typedef NTSTATUS(WINAPI* LdrGetProcedureAddressT)(IN PVOID DllHandle, IN PANSI_STRING ProcedureName OPTIONAL, IN ULONG ProcedureNumber OPTIONAL, OUT FARPROC* ProcedureAddress);
+	typedef VOID(WINAPI* RtlFreeUnicodeStringT)(_Inout_ PUNICODE_STRING UnicodeString);
+	typedef  VOID(WINAPI* RtlInitAnsiStringT)(_Out_    PANSI_STRING DestinationString, _In_opt_ PCSZ         SourceString);
+	typedef NTSTATUS(WINAPI* RtlAnsiStringToUnicodeStringT)(_Inout_ PUNICODE_STRING DestinationString, _In_ PCANSI_STRING SourceString, _In_ BOOLEAN AllocateDestinationString);
+	typedef NTSTATUS(WINAPI* LdrLoadDllT)(PWCHAR, PULONG, PUNICODE_STRING, PHANDLE);
+	typedef BOOL(APIENTRY* ProcDllMain)(LPVOID, DWORD, LPVOID);
+	typedef NTSTATUS(WINAPI* NtAllocateVirtualMemoryT)(IN HANDLE ProcessHandle, IN OUT PVOID* BaseAddress, IN ULONG ZeroBits, IN OUT PSIZE_T RegionSize, IN ULONG AllocationType, IN ULONG Protect);
+
+	struct PARAMX
+	{
+		PVOID lpFileData;
+		DWORD DataLength;
+		LdrGetProcedureAddressT LdrGetProcedureAddress;
+		NtAllocateVirtualMemoryT dwNtAllocateVirtualMemory;
+		LdrLoadDllT pLdrLoadDll;
+		RtlInitAnsiStringT RtlInitAnsiString;
+		RtlAnsiStringToUnicodeStringT RtlAnsiStringToUnicodeString;
+		RtlFreeUnicodeStringT RtlFreeUnicodeString;
+	};
+
+	DWORDX WINAPI MemLoadLibrary2(PARAMX* X);
+	DWORD InjectDLLEx(const std::string& fileName, DWORD pid);
+	std::pair<LPVOID, DWORD> GetMemLoadLibrary2ShellCode();
+}
